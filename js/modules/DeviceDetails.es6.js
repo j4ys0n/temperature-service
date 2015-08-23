92bb470dbacfd6a97969a3c6ecc1d30c0b4ef955
HUGE | Jayson Jacobs <jjacobs@hugeinc.com>
2015-08-23 00:10:22 -0600

    interactive device temperature chart

--- js/modules/DeviceDetails.es6.js
+++ js/modules/DeviceDetails.es6.js
@@ -1,395 +1,10 @@
-var mockData = [
-	{
-		"date": "2015-08-13T11:11:47-07:00",
-		"value": "30"
-	},
-	{
-		"date": "2015-08-13T03:56:52-07:00",
-		"value": "31"
-	},
-	{
-		"date": "2015-08-13T18:54:24-07:00",
-		"value": "32"
-	},
-	{
-		"date": "2015-08-13T17:08:52-07:00",
-		"value": "33"
-	},
-	{
-		"date": "2015-08-13T09:31:12-07:00",
-		"value": "34"
-	},
-	{
-		"date": "2015-08-13T20:32:33-07:00",
-		"value": "30"
-	},
-	{
-		"date": "2015-08-13T21:13:37-07:00",
-		"value": "31"
-	},
-	{
-		"date": "2015-08-13T22:18:40-07:00",
-		"value": "32"
-	},
-	{
-		"date": "2015-08-13T00:39:17-07:00",
-		"value": "33"
-	},
-	{
-		"date": "2015-08-13T11:45:43-07:00",
-		"value": "34"
-	},
-	{
-		"date": "2015-08-13T15:34:50-07:00",
-		"value": "30"
-	},
-	{
-		"date": "2015-08-13T17:14:57-07:00",
-		"value": "31"
-	},
-	{
-		"date": "2015-08-13T17:41:08-07:00",
-		"value": "32"
-	},
-	{
-		"date": "2015-08-13T19:11:11-07:00",
-		"value": "33"
-	},
-	{
-		"date": "2015-08-13T07:42:23-07:00",
-		"value": "34"
-	},
-	{
-		"date": "2015-08-13T03:59:35-07:00",
-		"value": "30"
-	},
-	{
-		"date": "2015-08-13T07:39:32-07:00",
-		"value": "31"
-	},
-	{
-		"date": "2015-08-13T16:07:33-07:00",
-		"value": "32"
-	},
-	{
-		"date": "2015-08-13T16:42:42-07:00",
-		"value": "33"
-	},
-	{
-		"date": "2015-08-13T18:59:50-07:00",
-		"value": "34"
-	},
-	{
-		"date": "2015-08-13T08:23:12-07:00",
-		"value": "30"
-	},
-	{
-		"date": "2015-08-13T04:49:16-07:00",
-		"value": "31"
-	},
-	{
-		"date": "2015-08-13T05:45:59-07:00",
-		"value": "32"
-	},
-	{
-		"date": "2015-08-13T10:02:43-07:00",
-		"value": "33"
-	},
-	{
-		"date": "2015-08-13T07:14:57-07:00",
-		"value": "34"
-	},
-	{
-		"date": "2015-08-13T04:22:03-07:00",
-		"value": "30"
-	},
-	{
-		"date": "2015-08-13T12:14:18-07:00",
-		"value": "31"
-	},
-	{
-		"date": "2015-08-13T22:22:35-07:00",
-		"value": "32"
-	},
-	{
-		"date": "2015-08-13T19:21:54-07:00",
-		"value": "33"
-	},
-	{
-		"date": "2015-08-13T01:53:35-07:00",
-		"value": "34"
-	},
-	{
-		"date": "2015-08-13T08:59:00-07:00",
-		"value": "30"
-	},
-	{
-		"date": "2015-08-13T22:52:37-07:00",
-		"value": "31"
-	},
-	{
-		"date": "2015-08-13T10:06:44-07:00",
-		"value": "32"
-	},
-	{
-		"date": "2015-08-13T07:49:58-07:00",
-		"value": "33"
-	},
-	{
-		"date": "2015-08-13T15:47:18-07:00",
-		"value": "34"
-	},
-	{
-		"date": "2015-08-13T01:59:10-07:00",
-		"value": "30"
-	},
-	{
-		"date": "2015-08-13T10:58:36-07:00",
-		"value": "31"
-	},
-	{
-		"date": "2015-08-13T01:34:25-07:00",
-		"value": "32"
-	},
-	{
-		"date": "2015-08-13T04:30:18-07:00",
-		"value": "33"
-	},
-	{
-		"date": "2015-08-13T09:29:55-07:00",
-		"value": "34"
-	},
-	{
-		"date": "2015-08-13T14:24:12-07:00",
-		"value": "30"
-	},
-	{
-		"date": "2015-08-13T02:22:04-07:00",
-		"value": "31"
-	},
-	{
-		"date": "2015-08-13T07:08:38-07:00",
-		"value": "32"
-	},
-	{
-		"date": "2015-08-13T21:14:03-07:00",
-		"value": "33"
-	},
-	{
-		"date": "2015-08-13T03:06:30-07:00",
-		"value": "34"
-	},
-	{
-		"date": "2015-08-13T21:02:15-07:00",
-		"value": "30"
-	},
-	{
-		"date": "2015-08-13T11:47:02-07:00",
-		"value": "31"
-	},
-	{
-		"date": "2015-08-13T05:03:06-07:00",
-		"value": "32"
-	},
-	{
-		"date": "2015-08-13T23:45:34-07:00",
-		"value": "33"
-	},
-	{
-		"date": "2015-08-13T23:07:13-07:00",
-		"value": "34"
-	},
-	{
-		"date": "2015-08-13T08:35:51-07:00",
-		"value": "30"
-	},
-	{
-		"date": "2015-08-13T03:01:41-07:00",
-		"value": "31"
-	},
-	{
-		"date": "2015-08-13T22:53:03-07:00",
-		"value": "32"
-	},
-	{
-		"date": "2015-08-13T18:45:18-07:00",
-		"value": "33"
-	},
-	{
-		"date": "2015-08-13T17:59:43-07:00",
-		"value": "34"
-	},
-	{
-		"date": "2015-08-13T00:49:36-07:00",
-		"value": "30"
-	},
-	{
-		"date": "2015-08-13T10:12:07-07:00",
-		"value": "31"
-	},
-	{
-		"date": "2015-08-13T19:26:02-07:00",
-		"value": "32"
-	},
-	{
-		"date": "2015-08-13T22:19:02-07:00",
-		"value": "33"
-	},
-	{
-		"date": "2015-08-13T14:25:43-07:00",
-		"value": "34"
-	},
-	{
-		"date": "2015-08-13T01:54:56-07:00",
-		"value": "30"
-	},
-	{
-		"date": "2015-08-13T04:53:27-07:00",
-		"value": "31"
-	},
-	{
-		"date": "2015-08-13T15:29:19-07:00",
-		"value": "32"
-	},
-	{
-		"date": "2015-08-13T21:11:29-07:00",
-		"value": "33"
-	},
-	{
-		"date": "2015-08-13T21:04:16-07:00",
-		"value": "34"
-	},
-	{
-		"date": "2015-08-13T10:02:09-07:00",
-		"value": "30"
-	},
-	{
-		"date": "2015-08-13T23:24:19-07:00",
-		"value": "31"
-	},
-	{
-		"date": "2015-08-13T17:24:06-07:00",
-		"value": "32"
-	},
-	{
-		"date": "2015-08-13T22:29:13-07:00",
-		"value": "33"
-	},
-	{
-		"date": "2015-08-13T14:07:11-07:00",
-		"value": "34"
-	},
-	{
-		"date": "2015-08-13T01:26:01-07:00",
-		"value": "30"
-	},
-	{
-		"date": "2015-08-13T00:35:22-07:00",
-		"value": "31"
-	},
-	{
-		"date": "2015-08-13T11:13:48-07:00",
-		"value": "32"
-	},
-	{
-		"date": "2015-08-13T04:44:20-07:00",
-		"value": "33"
-	},
-	{
-		"date": "2015-08-13T05:05:32-07:00",
-		"value": "34"
-	},
-	{
-		"date": "2015-08-13T17:33:57-07:00",
-		"value": "30"
-	},
-	{
-		"date": "2015-08-13T08:29:04-07:00",
-		"value": "31"
-	},
-	{
-		"date": "2015-08-13T18:00:06-07:00",
-		"value": "32"
-	},
-	{
-		"date": "2015-08-13T08:49:23-07:00",
-		"value": "33"
-	},
-	{
-		"date": "2015-08-13T09:04:42-07:00",
-		"value": "34"
-	},
-	{
-		"date": "2015-08-13T10:48:21-07:00",
-		"value": "30"
-	},
-	{
-		"date": "2015-08-13T22:26:56-07:00",
-		"value": "31"
-	},
-	{
-		"date": "2015-08-13T04:37:14-07:00",
-		"value": "32"
-	},
-	{
-		"date": "2015-08-13T10:36:42-07:00",
-		"value": "33"
-	},
-	{
-		"date": "2015-08-13T09:33:17-07:00",
-		"value": "34"
-	},
-	{
-		"date": "2015-08-13T03:32:08-07:00",
-		"value": "30"
-	},
-	{
-		"date": "2015-08-13T21:24:28-07:00",
-		"value": "31"
-	},
-	{
-		"date": "2015-08-13T17:08:50-07:00",
-		"value": "32"
-	},
-	{
-		"date": "2015-08-13T12:56:11-07:00",
-		"value": "33"
-	},
-	{
-		"date": "2015-08-13T14:16:23-07:00",
-		"value": "34"
-	},
-	{
-		"date": "2015-08-13T02:18:46-07:00",
-		"value": "30"
-	},
-	{
-		"date": "2015-08-13T09:08:27-07:00",
-		"value": "31"
-	},
-	{
-		"date": "2015-08-13T10:43:20-07:00",
-		"value": "32"
-	},
-	{
-		"date": "2015-08-13T06:15:48-07:00",
-		"value": "33"
-	},
-	{
-		"date": "2015-08-13T21:22:00-07:00",
-		"value": "34"
-	},
-	{
-		"date": "2015-08-13T09:10:08-07:00",
-		"value": "30"
-	}
-];
-
 import DocDetails from './DocDetails.es6';
 
 class DeviceDetails extends DocDetails {
 	constructor($, Utils) {
 		let d3 = require('d3'),
+			//j = require('jQuery'),
+			Rickshaw = require('rickshaw'),
 			utils = new Utils();
 
 			let constants = {
@@ -424,154 +39,73 @@ class DeviceDetails extends DocDetails {
 				submit: $(selectors.submit)
 			};
 
-		let createChart = function( data ){
-			var margin = {top: 20, right: 20, bottom: 30, left: 50},
-			    width = 960 - margin.left - margin.right,
-			    height = 500 - margin.top - margin.bottom;
+		let rickshawChart = function( data ) {
 
-			var parseDate = d3.time.format("%a %b %d %Y %I:%M:%S").parse;
+			data.sort(function(a,b){
+				return parseFloat(a.x) - parseFloat(b.x);
+			});
 
-			var x = d3.time.scale()
-			    .range([0, width]);
+			var graph = new Rickshaw.Graph( {
+				element: document.getElementById('chart'),
+				width: 960,
+				height: 500,
+				min: 50,
+				renderer: 'line',
+				series: [
+					{
+						color: '#c05020',
+						data: data,
+						name: 'Temperature'
+					}
+				]
+			});
+			graph.render();
 
-			var y = d3.scale.linear()
-			    .range([height, 0]);
+			var hoverDetail = new Rickshaw.Graph.HoverDetail({
+				graph: graph
+			});
 
-			var xAxis = d3.svg.axis()
-			    .scale(x)
-			    .orient("bottom");
+			var legend = new Rickshaw.Graph.Legend({
+				graph: graph,
+				element: document.getElementById('legend')
+			});
 
-			var yAxis = d3.svg.axis()
-			    .scale(y)
-			    .orient("left");
+			var axes = new Rickshaw.Graph.Axis.Time({
+				graph: graph
+			});
+			axes.render();
+			var y_axis = new Rickshaw.Graph.Axis.Y( {
+				graph:graph,
+				orientation: 'left',
+				element: document.getElementById('y_axis')
+			});
 
-			var line = d3.svg.line()
-			    .x(function(d) { return x(d.date); })
-			    .y(function(d) { return y(d.value); });
-
-			var svg = d3.select("#chart").append("svg")
-			    .attr("width", width + margin.left + margin.right)
-			    .attr("height", height + margin.top + margin.bottom)
-			  .append("g")
-			    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-			//d3.tsv("data.tsv", function(error, data) {
-			//  if (error) throw error;
-
-			//   data.forEach(function(d) {
-			//     d.date = parseDate(d);
-			//     d.close = +d.close;
-			// 	console.log(d);
-			//   });
-
-			// var newData = [];
-			//
-			// var lastValue;
-			//
-			//   for(var i = 0; i < 24; i++){
-			// 	  var iStr = (i < 10) ? String('0'+i) : String(i);
-			// 	  var hourly = data[0].temperatures.hourly[iStr];
-			// 	  //console.log(hourly);
-			// 	  for(var j = 0; j < 4; j++){
-			// 		 var date = Date.parse(hourly[j].time);
-			// 		 if(isNaN(date)){
-			// 			 date = lastValue;
-			// 		 }else{
-			// 			 lastValue = date;
-			// 		 }
-			//
-			// 		  //date = dateFormat(date, "isoDateTime");
-			// 		  var obj = {
-			// 			  date: date,
-			// 			  value: hourly[j].value
-			// 		  }
-			// 		  newData.push(obj);
-			// 	  }
-			//   }
-
-			//   data.forEach(function(d){
-			// 	  d.date = Date.parse(d.date);
-			// 	  d.value = +d.value;
-			//   });
-
-			  data.sort(function(a,b){
-				  return parseFloat(a.date) - parseFloat(b.date);
-			  });
-
-			  //data = newData;
-			  //data = mockData;
-			  //console.log(data);
-
-			//   data.forEach(function(d) {
-			//     d.date = parseDate(d);
-			//     d.value = d.value;
-			// 	//console.log(d);
-			//   });
-
-			  x.domain(d3.extent(data, function(d) { return d.date; }));
-			  //y.domain(d3.extent(data, function(d) { return d.value; }));
-			  y.domain([20, 100]);
-
-			  svg.append("g")
-			      .attr("class", "x axis")
-			      .attr("transform", "translate(0," + height + ")")
-			      .call(xAxis);
-
-			  svg.append("g")
-			      .attr("class", "y axis")
-			      .call(yAxis)
-			    .append("text")
-			      .attr("transform", "rotate(-90)")
-			      .attr("y", 6)
-			      .attr("dy", ".71em")
-			      .style("text-anchor", "end")
-			      .text("Temperature");
-
-			  svg.append("path")
-			      .datum(data)
-			      .attr("class", "line")
-			      .attr("d", line);
-			//});
-		}
+			y_axis.render();
+		};
 
 		let dateFormatter = function(dt) {
-
-			let leadingZero = function(val) {
-				var rtn = val.toString();
-				if(val < 10){
-					rtn = '0'+val.toString();
-				}
-				return rtn;
-			}
-
-			//var dStr = dt.getFullYear()+'-'+leadingZero((dt.getMonth()+1))+'-'+leadingZero(dt.getDate())+'T'+leadingZero((dt.getHours()+1))+':'+leadingZero(dt.getMinutes())+':'+leadingZero(dt.getSeconds())+'-00:00';
-			//Date.UTC(year,month,day,hours,minutes,seconds,millisec)
-			var dStr = Date.UTC(dt.getFullYear(), dt.getMonth(), dt.getDate(), dt.getHours(), dt.getMinutes(), dt.getSeconds());
-			return dStr;
+			return Date.UTC(dt.getFullYear(), dt.getMonth(), dt.getDate(), dt.getHours(), dt.getMinutes(), dt.getSeconds());
 		}
 
-		let testDataHandler = function( res ){
-
+		let temperatureRequestHandler = function( res ){
 			var data = JSON.parse(res).data,
 				chartData = [];
-			utils.debugConsole(data);
 			for( var i = 0; i < data.length; i++ ){
 				var doc = data[i],
 					temps = doc.temperatures.hourly;
 				for( var hour in temps ) {
 					for( var interval in temps[hour] ){
-						//utils.debugConsole(temps[hour][interval]);
 						if(temps[hour][interval].time != ''){
-							var dt = new Date(temps[hour][interval].time),
-								dtStr = dateFormatter(dt);
-
-							chartData.push({date: dtStr, value: temps[hour][interval].value });
+							var dt = dateFormatter(new Date(temps[hour][interval].time));
+							//chartData.push({date: dt, value: temps[hour][interval].value });
+							chartData.push({x: dt, y: temps[hour][interval].value });
 						}
 
 					}
 				}
 			}
-			createChart(chartData);
+			console.log(chartData);
+			rickshawChart(chartData);
 		};
 
 		let deviceRequestHandler = function( res ){
@@ -579,23 +113,14 @@ class DeviceDetails extends DocDetails {
 			utils.debugConsole(device);
 		};
 
-		let temperatureRequestHandler = function( res ){
-			var temps = JSON.parse(res).data;
-			//console.log(temps);
-			//createChart(temps);
-		};
-
 		super($, Utils, forms, {delete: constants.deleteUrl,
 								update: constants.updateUrl });
 
 		this.firstRun = function() {
 			utils.loadUrl( constants.getDeviceURL+id, 'GET', null, false, deviceRequestHandler );
-			utils.loadUrl( constants.getTempsURL+id, 'GET', null, false, temperatureRequestHandler );
-			//console.log(d3);
 
-
-			//utils.loadUrl('http://52.20.3.36/api/temperature/device/all/55d2a1628dfc55c704d6aa8d', 'GET', null, false, testDataHandler);
-			utils.loadUrl('/api/temperature/device/all/'+id, 'GET', null, false, testDataHandler);
+			//utils.loadUrl('http://52.20.3.36/api/temperature/device/all/55d2a1628dfc55c704d6aa8d', 'GET', null, false, temperatureRequestHandler);
+			utils.loadUrl('/api/temperature/device/all/'+id, 'GET', null, false, temperatureRequestHandler);
 		};
 	}
 
